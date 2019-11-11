2deba6ebda8ed9dfb6c2863b2f53b9388275bc1e
denizyesilirmak <denizyesilirmak@gmail.com>
2019-11-11 14:21:11 +0300

    FIX: audio sample rate

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Telegraf = require('telegraf')
+const extra = require('telegraf/extra')
 const Telegram = require('telegraf/telegram')
 const https = require('https')
 
 const speech = require('@google-cloud/speech');
 const fs = require('fs');
 const client = new speech.SpeechClient();
-
 
 
 require('dotenv').config()
@@ -16,43 +16,53 @@ const telegram = new Telegram(process.env.API_KEY)
 const getFile = async (fileId) => {
   let url = await telegram.getFileLink(fileId)
   return new Promise((resolve, reject) => {
-    https.get(url, res => {
-      res.pipe(client
-        .streamingRecognize(request)
-        .on('error', err => reject)
-        .on('data', data => {
-            resolve(data.results[0].alternatives[0].transcript)
-        })
-      )
-      setTimeout(() => {
-        reject("timeout")
-      }, 5000);
+    https.get(url, async res => {
+      const chunks = []
+      res.on('data', (d) => {
+        chunks.push(d)
+      })
+      res.on('end', async () => {
+        const file = Buffer.concat(chunks)
+        const transcription = await transcribe(file)
+        resolve(transcription)
+      })
+    }).on('error', (e) => {
+      reject('http get error')
     })
   })
 }
 
-const request = {
-  config: {
-    encoding: 'OGG_OPUS',
-    sampleRateHertz: 48000,
-    languageCode: 'tr_TR',
-  },
-  interimResults: false
-}
+const transcribe = async (file) => {
+  const audioBytes = file.toString('base64');
 
+  const audio = {
+    content: audioBytes,
+  }
+  const config = {
+    encoding: 'OGG_OPUS',
+    sampleRateHertz: 16000,
+    languageCode: 'tr-TR'
+  }
+  const request = {
+    audio: audio,
+    config: config
+  }
+
+  const [ results ] = await client.recognize(request)
+
+  return results.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n')
+}
 
 bot.on('voice', async (ctx) => {
   let file
   try {
     file = await getFile(ctx.message.voice.file_id)
-    ctx.reply(file)
+    ctx.reply(file, extra.inReplyTo(ctx.message.message_id))
   } catch (error) {
     console.log(error)
   }
-
-  
 })
 
-bot.launch()
-
-
+bot.startPolling()
--- main.js
+++ main.js
@@ -1,15 +1,39 @@
+
+
 async function main() {
+  // Imports the Google Cloud client library
+  const speech = require('@google-cloud/speech');
+  const fs = require('fs');
 
+  // Creates a client
+  const client = new speech.SpeechClient();
 
-    const fileName = './resources/yihit.ogg';
+  // The name of the audio file to transcribe
+  const fileName = './resources/file_118.oga';
 
-    const file = fs.readFileSync(fileName);
-    const audioBytes = file.toString('base64');
-  
+  // Reads a local audio file and converts it to base64
+  const file = fs.readFileSync(fileName);
+  const audioBytes = file.toString('base64');
 
-    const audio = {
-      content: audioBytes,
-    };
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: 'OGG_OPUS',
+    sampleRateHertz: 48000,
+    languageCode: 'tr-TR',
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
 
-  }
-  main().catch(console.error);
+  // Detects speech in the audio file
+  const [response] = await client.recognize(request);
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+  console.log(`Transcription: ${transcription}`);
+}
+main().catch(console.error);
